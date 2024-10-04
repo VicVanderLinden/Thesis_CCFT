86b6b1d1783101dfea4255cc852ce6b400e6f6ad
VicVanderLinden <Vic.Vander.linden@gmail.com>
2024-10-04 10:50:03 +0200

    Week5do

--- MPSkit Potts Model Investigation.jl
+++ MPSkit Potts Model Investigation.jl
@@ -2,50 +2,6 @@
 # https://arxiv.org/pdf/2403.00852 (2024)
 
 using MPSKitModels, TensorKit
-
-"""
-    potts_exchange([eltype::Type{<:Number}], [symmetry::Type{<:Sector}]; q=3)
-
-The Potts exchange operator ``Z ⊗ Z' + Z' ⊗ Z``, where ``Z^q = 1``.
-"""
-function potts_exchange end
-potts_exchange(; kwargs...) = potts_exchange(ComplexF64, Trivial; kwargs...)
-potts_exchange(elt::Type{<:Number}; kwargs...) = potts_exchange(elt, Trivial; kwargs...)
-function potts_exchange(symmetry::Type{<:Sector}; kwargs...)
-    return potts_exchange(ComplexF64, symmetry; kwargs...)
-end
-
-function potts_exchange(elt::Type{<:Number}, ::Type{Trivial}; q=3)
-    pspace = ComplexSpace(q)
-    Z = TensorMap(zeros, elt, pspace ← pspace)
-    for i in 1:q
-        Z[i, i] = cis(2π * (i - 1) / q)
-    end
-    return Z ⊗ Z' + Z' ⊗ Z
-end
-"""
-    potts_field([eltype::Type{<:Number}], [symmetry::Type{<:Sector}]; q=3) 
-
-The Potts field operator ``X + X'``, where ``X^q = 1``.
-"""
-
-function potts_field end
-potts_field(; kwargs...) = potts_field(ComplexF64, Trivial; kwargs...)
-potts_field(elt::Type{<:Number}; kwargs...) = potts_field(elt, Trivial; kwargs...)
-function potts_field(symmetry::Type{<:Sector}; kwargs...)
-    return potts_field(ComplexF64, symmetry; kwargs...)
-end
-
-function potts_field(elt::Type{<:Number}, ::Type{Trivial}; q=3)
-    pspace = ComplexSpace(q)
-    X = TensorMap(zeros, elt, pspace ← pspace)
-    for i in 1:q
-        X[mod1(i - 1, q), i] = one(elt)
-    end
-    return X + X'
-end
-
-
 """
     potts_spin_shift([eltype::Type{<:Number}], [symmetry::Type{<:Sector}]; q,k)
 
@@ -107,7 +63,7 @@ using JLD2
 
 J = 1
 h = 1
-Q = 5 ### Run it for Q=3 and Q = 5 and you see that they will repectively agree and not agree
+Q = 5### Run it for Q=3 and Q = 5 and you see that they will repectively agree and not agree
 D=10
 L_list = 6:4:22
 
@@ -117,9 +73,8 @@ energies = similar(L_list,ComplexF64)
 global i=0
 for L in L_list
     ###Potts Hamiltonian
-    H_potts = quantum_potts(FiniteChain(L),q=Q,J=J,g=1)
-   
-    H = periodic_boundary_conditions(H_potts); #### not sure if these work (they don't change anything for the MPSKit/Homemade hamiltonian)
+    H = quantum_potts(FiniteChain(L),q=Q)
+
     global i+=1
     ψ₀ = FiniteMPS(L,ℂ^Q, ℂ^D);
     println("start")
@@ -133,26 +88,28 @@ for j in 1:1:length(L_list)
 end
 
 p = plot(; xlabel="1/L²", ylabel="Re(E0/L)")
-plot!(x_values,real(divided_energies) ; seriestype=:scatter,label="MPSKitModel")
+plot!(x_values,real(divided_energies) ; seriestype=:scatter,label="Potts_no_boundary")
 energies = similar(L_list,ComplexF64)
 global i=0
 for L in L_list
     ###Yin Tang hamiltonian
-    H_potts = @mpoham sum( (-h * potts_spin_shift(; q = Q,k=j)){vertices(FiniteChain(L))[end]} for j in 1:1:Q-1)+ sum(sum((-J * potts_phase(; q=Q,k=j)){i,i+1} - (h * potts_spin_shift(; q = Q,k=j)){i} for j in 1:1:Q-1) for i in vertices(FiniteChain(L))[1:(end - 1)]); 
-    H = periodic_boundary_conditions(H_potts);
-    global i+=1
+    H = @mpoham (sum(sum((-J * potts_phase(; q=Q,k=j)){i,i+1} + (-h * potts_spin_shift(; q = Q,k=j)){i} for j in 1:1:Q-1) for i in vertices(FiniteChain(L))[1:(end - 1)]) ##" potts model w no open bc
+    +sum(-h * potts_spin_shift(; q = Q,k=j){vertices(FiniteChain(L))[end]} for j in 1:1:Q-1))
     ψ₀ = FiniteMPS(L,ℂ^Q, ℂ^D);
     println("start")
+    global i+=1
     ψ, envs , delta   = find_groundstate(ψ₀, H, DMRG(maxiter = 500,tol=1e-5, eigalg =MPSKit.Defaults.alg_eigsolve(; ishermitian=false)));
     energies[i] = expectation_value(ψ,H,envs)
 end
+
+
 x_values = 1 ./L_list.^2
 divided_energies = similar(L_list,ComplexF64)
-for j in 1:1:length(L_list)
+for j in 1:1:length(L_list) 
     divided_energies[j] = energies[j]/L_list[j]
 end
-
-plot!(x_values,real(divided_energies) ; seriestype=:scatter,label="Ying_tangH0_model")
+println("here")
+plot!(x_values,real(divided_energies) ; seriestype=:scatter,label="Yin_tang_no_boundary")
 savefig(p,"Energy scaling_PottsMPSvsHomemade D = $D, Q = $Q.png")
 
 
@@ -161,6 +118,7 @@ savefig(p,"Energy scaling_PottsMPSvsHomemade D = $D, Q = $Q.png")
 
 
 
+
 ### conculsion this is the true potts hamiltonian with open boundary conditions
-H_potts = @mpoham sum( (-h * potts_spin_shift(; q = Q,k=j)){vertices(FiniteChain(L))[end]} for j in 1:1:Q-1)+ sum(sum((-J * potts_phase(; q=Q,k=j)){i,i+1} - (h * potts_spin_shift(; q = Q,k=j)){i} for j in 1:1:Q-1) for i in vertices(FiniteChain(L))[1:(end - 1)]); 
+#H_potts = @mpoham sum( (-h * potts_spin_shift(; q = Q,k=j)){vertices(FiniteChain(L))[end]} for j in 1:1:Q-1)+ sum(sum((-J * potts_phase(; q=Q,k=j)){i,i+1} - (h * potts_spin_shift(; q = Q,k=j)){i} for j in 1:1:Q-1) for i in vertices(FiniteChain(L))[1:(end - 1)]); 
    
