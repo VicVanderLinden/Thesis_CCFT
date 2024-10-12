9608aeb23aa804a2d8ea30faf635d40c7bc8148d
VicVanderLinden <Vic.Vander.linden@gmail.com>
2024-10-12 11:11:19 +0200

    Week6

--- Non_hermitian_model symm.jl
+++ Non_hermitian_model symm.jl
@@ -24,7 +24,7 @@ function potts_spin_shift(elt::Type{<:Number}, ::Type{Trivial}; q=3, k=1)
     for i in 1:q
         tau[i,mod1(i - 1, q)] = one(elt)
     end
-    return TensorMap(((P_in * tau* P)^k).data,Vp←Vp)                         ### nonzero elements on the parts that intermingle between sectors
+    return (tau^k).data                        ### nonzero elements on the parts that intermingle between sectors
 end
 function potts_phase end
 potts_phase(; kwargs...) = potts_phase(ComplexF64, Trivial; kwargs...)
@@ -40,7 +40,7 @@ function potts_phase(elt::Type{<:Number}, ::Type{Trivial}; q=3,k=1)
     for i in 1:q
         sigma[i, i] = cis(2*pi*(i-1)/q)
     end
-    return TensorMap((((P_inv*sigma*P)'⊗ (P_inv*sigma*P))^k).data,Vp⊗Vp←Vp⊗Vp)  ### nonzero elements on the parts that intermingle between sectors
+    return ((sigma'⊗ sigma)^k).data  
 end
 
 function potts_phase_shift_combined end
@@ -61,9 +61,7 @@ function potts_phase_shift_combined(elt::Type{<:Number}, ::Type{Trivial}; q=3,k=
         tau[i,mod1(i - 1, q)] = one(elt)
         identity_e[i,i]= 1
     end
-    tau_n = P_in * tau* P
-    sigma_n = P_in * sigma* P
-    return TensorMap(((tau_n^k'⊗ identity_e) * (sigma_n'⊗ sigma_n)^p + (identity_e ⊗ tau_n^k') * (sigma_n'⊗ sigma_n)^k + (sigma_n'⊗ sigma_n)^k * (tau_n^p'⊗ identity_e) +  (sigma_n'⊗ sigma_n)^k * (identity_e ⊗ tau_n^p')).data,Vp⊗Vp←Vp⊗Vp)
+    return (tau^k'⊗ identity_e) * (sigma'⊗ sigma)^p + (identity_e ⊗ tau^k') * (sigma'⊗ sigma)^k + (sigma'⊗ sigma)^k * (tau^p'⊗ identity_e) +  (sigma'⊗ sigma)^k * (identity_e ⊗ tau^p')
 end
 
 
@@ -74,72 +72,53 @@ lambda = 0.079 + 0.060im
 J = 1
 h = 1
 Q = 5
-D= 50
+D= 100
 
 
 ### symmetry
 using LinearAlgebra     
-Vp = Vect[ZNIrrep{5}](0=>1,1=>1,2=>1,3=>1,4 =>1)
-tau = zeros(ComplexF64,Q,Q) 
-for i in 1:Q
-    tau[i,mod1(i - 1, Q)] = 1
-end
-eigenv = eigvecs(tau)       
-pspace = ComplexSpace(Q)    
-P = TensorMap(eigenv, pspace ← pspace)      
-P_inv = TensorMap(inv(eigenv), pspace ← pspace) 
+Vp = Vect[ZNIrrep{Q}](0=>1,1=>1,2=>1,3=>1,4=>1)         
+T = zeros(ComplexF64,Q,Q)           
+ω = exp(2*π*im/Q)   
+for i in 0:Q-1
+    for j in 0:Q-1
+        T[i+1,j+1] = (ω^i)^j
+    end
+end 
+P = TensorMap(T/sqrt(5),ℂ^Q←ℂ^Q)       
+pspace = ComplexSpace(Q)                     
+P_inv = TensorMap(inv(P.data), pspace ← pspace)        
 
 
-L = 10
-
-H = @mpoham (sum(sum((-J * potts_phase(; q=Q,k=j)){i,i+1} + (-h * potts_spin_shift(; q = Q,k=j)){i} for j in 1:1:Q-1) for i in vertices(FiniteChain(L))[1:(end - 1)]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                     ##### Simulating energies
-L_list = 8:1:30
+                                    ##### Simulating energies
+L_list = 8:1:13
 N_sizes = length(L_list)
 N_levels = 1 ## Gets until the N'th energie level
 Energie_levels = zeros(ComplexF64,(N_sizes,N_levels+1))
 
-run = false
+run = true
 if run
 for (i,L) in enumerate(L_list)
 
     ###Yin Tang hamiltonian
-    H = @mpoham (sum(sum((-J * potts_phase(; q=Q,k=j)){i,i+1} + (-h * potts_spin_shift(; q = Q,k=j)){i} for j in 1:1:Q-1) for i in vertices(FiniteChain(L))[1:(end - 1)]) ##" potts model
-    +sum( -J * potts_phase(; q=Q,k=j){vertices(FiniteChain(L))[end],vertices(FiniteChain(L))[1]}+ (-h * potts_spin_shift(; q = Q,k=j)){vertices(FiniteChain(L))[end]} for j in 1:1:Q-1) ##potts model periodic bc
-    + lambda * sum( sum(sum(potts_phase_shift_combined(;q=Q,k=j,p=l){i,i+1} for l in 1:1:Q-1) for j in 1:1:Q-1)   for i in vertices(FiniteChain(L))[1:(end - 1)]) ##" additional non hermitian model
-    + lambda * sum(sum(potts_phase_shift_combined(;q=Q,k=j,p=l){vertices(FiniteChain(L))[end],vertices(FiniteChain(L))[1]} for l in 1:1:Q-1) for j in 1:1:Q-1)); ## non hermitian model periodic bc
-    ψ₀ = FiniteMPS(L,ℂ^Q, ℂ^D);
+    H0 = @mpoham (sum(TensorMap(P_inv.data*sum((-h * potts_spin_shift(; q = Q,k=j))*P.data for j in 1:1:Q-1),Vp←Vp){i} for i in vertices(FiniteChain(L))[1:(end)])) ### Potts
+    H1 = @mpoham (sum(TensorMap((P_inv ⊗ P_inv).data*sum((-J * potts_phase(; q=Q,k=j)) for j in 1:1:Q-1)*(P ⊗ P).data, Vp⊗Vp←Vp⊗Vp){i,i+1}  for i in vertices(FiniteChain(L))[1:(end-1)]) + TensorMap((P_inv ⊗ P_inv).data*sum((-J * potts_phase(; q=Q,k=j)) for j in 1:1:Q-1)*(P ⊗ P).data, Vp⊗Vp←Vp⊗Vp){vertices(FiniteChain(L))[end],vertices(FiniteChain(L))[1]}) ##¨Potts with BC
+    H2 =  @mpoham lambda * sum( TensorMap(((P_inv ⊗ P_inv) * sum(sum(potts_phase_shift_combined(;q=Q,k=l,p=j) for l in 1:1:Q-1) for j in 1:1:Q-1) *(P⊗P)).data,Vp⊗Vp←Vp⊗Vp){i,i+1}   for i in vertices(FiniteChain(L))[1:(end - 1)]) + lambda * sum(TensorMap(((P_inv ⊗ P_inv) * sum(potts_phase_shift_combined(;q=Q,k=l,p=j) for l in 1:1:Q-1) *(P⊗P)).data, Vp⊗Vp←Vp⊗Vp ) for j in 1:1:Q-1){vertices(FiniteChain(L))[end],vertices(FiniteChain(L))[1]} ###Extra term
+    H = H0+H1+H2
+    Vleft = Vect[ZNIrrep{5}](0=>1)
+    Vright = Vect[ZNIrrep{5}](1=>1)
+    ψ₀ = FiniteMPS(L,Vp,Vect[ZNIrrep{Q}](0=>D,1=>D,2=>D,3=>D,4=>D)) #;left=Vleft, right=Vright)
+
     println("start")
-    (ψ, envir , delta)   = find_groundstate(ψ₀, H, DMRG(maxiter = 500,tol=1e-7, eigalg =MPSKit.Defaults.alg_eigsolve(; ishermitian=false)));
+    (ψ, envir , delta)   = find_groundstate(ψ₀, H, DMRG(maxiter = 500,tol=1e-6, eigalg =MPSKit.Defaults.alg_eigsolve(; ishermitian=false)))
     Energie_levels[i,1] = expectation_value(ψ,H,envir)
     states = (ψ, )
     if N_levels !=1
       
-        En , other  = excitations(H,FiniteExcited(gsalg =DMRG(maxiter = 200,tol=1e-3, eigalg =MPSKit.Defaults.alg_eigsolve(; ishermitian=false))), states,num=N_levels);
+        En , other  = excitations(H,FiniteExcited(gsalg =DMRG(maxiter = 200,tol=1e-6, eigalg =MPSKit.Defaults.alg_eigsolve(; ishermitian=false))), states,num=N_levels);
         Energie_levels[i,2:end] = En
     end
 end
 using JLD2
-save_object("MPSNonHermitian_pottsq5VicVanderLinden-$N_levels,_energies.jld2", Energie_levels)
+save_object("MPSNonHermitian_pottsq5excited-N$N_levels,D$D,energies.jld2", Energie_levels)
 end
