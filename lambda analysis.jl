dd67a67a877ac53e8a0463907955ac12b8b3f686
VicVanderLinden <Vic.Vander.linden@gmail.com>
2025-01-14 10:38:47 +0100

    test-value_improvment

--- lambda analysis.jl
+++ lambda analysis.jl
@@ -1,33 +1,51 @@
 ### lambda analysis
 using JLD2
 
-L = 8
-N = 6
-test_values = zeros(ComplexF64,(N*N))
-for i in 1:1:N
-    for j in 1:1:N
-        test_values[i+(j-1)*N] = 0.12-(0.1/N )* i + (0.02 + 0.1/N * (j-1))*im
+N = 5
+L = 6
+test_values = zeros(ComplexF64,(2*N-1)^2)
+### changed this slightly to allow for any parameter N to cross at 0.079 + 0.060i point -> its 2N-1 parameter square now (for any N)
+for i in 1:1:(2*N-1)
+    for j in 1:1:(2*N-1)
+        if i <N+1
+            if j<N+1
+                test_values[i+(j-1)*(2*N-1)] =  (LinRange(-0.04,0.00,N)[i]) .+ (0.079)  + 1im*LinRange(-0.040,0.00,N)[j] .+ 0.06im 
+            else 
+                test_values[i+(j-1)*(2*N-1)] =  (LinRange(-0.04,0.00,N)[i]) .+ (0.079)  + 1im*LinRange(0+0.040/(N-1),0.040+0.040/(N-1),N)[j-N] .+ 0.06im 
+            end
+        else
+            if j<N+1
+                test_values[i+(j-1)*(2*N-1)] =  (LinRange(0+0.040/(N-1),0.040+0.040/(N-1),N)[i-N]) .+ (0.079)  + 1im*LinRange(-0.040,0.00,N)[j] .+ 0.06im 
+            else
+                test_values[i+(j-1)*(2*N-1)] =  (LinRange(0+0.040/(N-1),0.040+0.040/(N-1),N)[i-N]) .+ (0.079)  + 1im*LinRange(0+0.040/(N-1),0.040+0.040/(N-1),N)[j-N] .+ 0.06im 
+            end
+        end 
+       
     end
 end
 
 
-
-################### Lambda istelf ###################
-# using PlotlyJS
-# results = load_object("Lambda_est_ge8.jld2")
-# p = PlotlyJS.plot(PlotlyJS.contour(   z=broadcast(log,broadcast(abs,results)),
-#     x=real(-im*test_values),
-#         y=real(test_values),fill=true,colorbar=attr(
-#             title="log(|gε'|)", # title here
-#             titleside="top",
-#             titlefont=attr(
-#                 size=14,
-#                 family="Arial, sans-serif"
-#             )
-#         )),Layout(title=attr(text = "L=$L",x = 0.5))
-# )
-# println(results)
-# savefig(p,"lambda_estimation_D100,L$L,N6dif.png")  
+################## Lambda istelf ###################
+using PlotlyJS
+results = load_object("alt_term_Lambda_est_ge6.jld2")
+z_values = zeros(length(results))
+print(results)
+for res in 1:1:length(results)
+    z_values[res] = log(abs(results[res]))
+end
+p = PlotlyJS.plot(PlotlyJS.contour(   z=z_values,
+     x=real(-im*test_values),
+         y=real(test_values),fill=true,colorbar=attr(
+             title="log(|gε'|)", # title here
+             titleside="top",
+             titlefont=attr(
+                 size=14,
+                 family="Arial, sans-serif"
+             )
+         )),Layout(title=attr(text = "L=$L",x = 0.5),xaxis_title="Im(λ)",yaxis_title="Re(λ)")
+ )
+println(results)
+savefig(p,"falt_term_ge__lambda_estimation_D50,L$L,N5.png")  
 
 
 
@@ -142,10 +160,31 @@ end
 
 
 
-### RG flow
-###
-lambda_est = Vector{ComplexF64}[]
+# ### RG flow (of dimensions)
+using Plots
 L_list = [6,8,10,12]
-for L in L_list
-    push!(lambda_est,load_object("data_upto_12/Lambda_est_ge$L.jld2"))
+data_Eε = Matrix{ComplexF64}(undef,length(L_list),(2*N-1)*(2*N-1))
+data_Eσ = Matrix{ComplexF64}(undef,length(L_list),(2*N-1)*(2*N-1))
+data_g = Matrix{ComplexF64}(undef,length(L_list),(2*N-1)*(2*N-1))
+
+for (i,L) in enumerate(L_list)
+    temp = load_object("Vic/ΔEε with lambda for $L.jld2")
+    temp2 = load_object("Vic/ΔEσ with lambda for$L.jld2")
+    temp3 = load_object("Vic/Lambda_est_ge$L.jld2")
+    for j in 1:N*N
+        data_g[i,j] = temp3[j]
+        data_Eσ[i,j] = temp2[j]
+        data_Eε[i,j] = temp[j]
+    end
 end
+p = plot(; xlabel="real(ge)", ylabel="im(ge))",title = "ge evolution L = [6,8,10,12]",legend=false)
+for i in 1:1:N*N
+    plot!(p,real(data_g[:,i]),real(-im.*data_g[:,i]),color="blue")
+    quiver!(p,[real(data_g[2,i]),],[real(-im.*data_g[2,i]);], quiver = [0.1.*(real(data_g[3,i]-data_g[2,i]),real(-im.*(data_g[3,i]-data_g[2,i]))),],color="red")
+
+end
+plot!(p,[0.00,],[0.000,], seriestype=:scatter)
+# savefig(p,"ge_evolution.png")
+
+println(data_g)
+println(test_values)
