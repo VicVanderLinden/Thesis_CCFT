124b22cee9a3ab6568564ab3e0a06b7b89a7fc17
VicVanderLinden <Vic.Vander.linden@gmail.com>
2025-03-24 14:24:27 +0100

    lambda_est_final

--- lambda estimation.jl
+++ lambda estimation.jl
@@ -21,7 +21,7 @@ using Optim
 ΔL1σ =1.1336 − 0.0205im
 Cε_primeσσ = 0.0658 + 0.0513im 
 Cε_primeεε = 0.8791 − 0.1404im
-Δε_prime = 1.908 − 0.599im
+Δε_prime = 1.9083 − 0.5987im
 AL1εε_prime = 1+Δε_prime*(Δε_prime -2)/(4*Δε)
 AL1σε_prime = 1+Δε_prime*(Δε_prime -2)/(4*Δσ)
 
@@ -36,11 +36,13 @@ function lambda_estimation(L,lambda_range,alg)
 
     ## sector search
     ψ = FiniteMPS(L,Vp,Vect[ZNIrrep{Q}](0=>D,1=>D,2=>D,3=>D,4=>D))
+    if alg == "DMRG"
     ψ2 =FiniteMPS(L,Vp,Vect[ZNIrrep{Q}](0=>D,1=>D,2=>D,3=>D,4=>D))
     ψ3 =FiniteMPS(L,Vp,Vect[ZNIrrep{Q}](0=>D,1=>D,2=>D,3=>D,4=>D),left= Vect[ZNIrrep{Q}](0=>1,1=>0,2=>0,3=>0,4=>0) , right=Vect[ZNIrrep{Q}](0=>1,1=>0,2=>0,3=>0,4=>0))
     ψ4 =FiniteMPS(L,Vp,Vect[ZNIrrep{Q}](0=>D,1=>D,2=>D,3=>D,4=>D))
     ψ5 =FiniteMPS(L,Vp,Vect[ZNIrrep{Q}](0=>D,1=>D,2=>D,3=>D,4=>D))
     ψ6 =FiniteMPS(L,Vp,Vect[ZNIrrep{Q}](0=>D,1=>D,2=>D,3=>D,4=>D),left= Vect[ZNIrrep{Q}](0=>1,1=>0,2=>0,3=>0,4=>0) , right=Vect[ZNIrrep{Q}](0=>1,1=>0,2=>0,3=>0,4=>0))
+    end
     for (i,lambda) in enumerate(lambda_range)
         H = Potts_Hamiltonian(L;lambda = lambda)
         (ψ, envir , delta)   = find_groundstate(ψ, H, DMRG(maxiter = 500,tol=1e-6, eigalg =MPSKit.Defaults.alg_eigsolve(; ishermitian=false)))
@@ -53,10 +55,13 @@ function lambda_estimation(L,lambda_range,alg)
             En0, st0 = excitations(H, QuasiparticleAnsatz(ishermitian=false), ψ, envir; sector=ZNIrrep{5}(0),num=2)
             En1, st1 = excitations(H, QuasiparticleAnsatz(ishermitian=false), ψ, envir;  sector=ZNIrrep{5}(1),num=2)
             En = vcat(En1,En0)
-            println(En)
+            ΔEε = En[3]
+            ΔEL1ε = En[4]
+            ΔEσ = En[1]
+            ΔEL1σ = En[2]
             st = vcat(st1,st0)
             stt = vcat([ψ,],st)
-            save_object("5statesQP$lambda.jld2", stt)
+            save_object("OBC L = $L,5statesQP$lambda.jld2", stt)
         elseif alg == "ChepigaAnsatz"
             println("Doesn't work, only targets sector 0 for some reason")
             Ground_energy = expectation_value(ψ,H)
@@ -105,8 +110,8 @@ function lambda_estimation(L,lambda_range,alg)
         EL1ε[i] = ΔEL1ε
         EL1σ[i] = ΔEL1σ
     end
-    save_object("$alg Lambda_est_ge_woc$L.jld2", gε_prime_wo_C)
-    save_object("$alg Lambda_est_ge$L.jld2", gε_prime)
+    save_object("$alg OBC Lambda_est_ge_woc$L.jld2", gε_prime_wo_C)
+    save_object("$alg OBC Lambda_est_ge$L.jld2", gε_prime)
     save_object("$alg ΔEε with lambda for $L.jld2",Eε)
     save_object("$alg ΔEσ with lambda for$L.jld2", Eσ)
     save_object("$alg ΔEl1ε with lambda for$L.jld2", EL1ε)
@@ -117,10 +122,10 @@ N = 2
 D = 50
 test_values = zeros(ComplexF64,(2*N-1)^2)
 l = length(test_values)
-distx = 0.01## distance from alleged fixed point 0.079+0.060i in real
-disty = 0.01# distance from alleged fixed point 0.079+0.060i in imaginary
-cent_im = 0.060im
-cent_r = 0.079
+distx = 0.001## distance from alleged fixed point 0.079+0.060i in real
+disty = 0.001# distance from alleged fixed point 0.079+0.060i in imaginary
+cent_im = 0.058im
+cent_r = 0.078
 
 ## snake like structure of test_values will allow for faster convergence when recycling ψ (because you don't jump the entire distx after the loop)
 for i in 1:1:(2*N-1)
@@ -164,5 +169,5 @@ end
 
 ## run simulation here -> chose length scales
 for L in [8,]
-    lambda_estimation(L,test_values,"DMRG")
+    lambda_estimation(L,test_values,"QuasiparticleAnsatz")
 end
