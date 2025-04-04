6c7ea15e8db1da17e4fa5c2a27ce024af0f7b1e4
Boris De Vos <boris.devos@ugent.be>
2025-04-04 10:33:50 +0200

    split up jobs in hpc

---
+++ HPC stuff/CCFTlambda/lambda_gs_sweepscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -m a
+#PBS -l walltime=2:00:00
+#PBS -l nodes=1:ppn=4
+#PBS -l mem=8GB
+#PBS -o $VSC_DATA/Vic/Lambda_estimation/Sweep/Output/stdout.$PBS_JOBID
+#PBS -e $VSC_DATA/Vic/Lambda_estimation/Sweep/Error/GS/stderr.$PBS_JOBID
+#
+
+STARTDIR=$VSC_DATA/Vic/Lambda_estimation/Sweep #$PBS_O_WORKDIR for Home Directory
+
+module load Julia/1.10.4-linux-x86_64
+
+export I_MPI_COMPATIBILITY=4
+
+# Move to proper working directory
+cd $PBS_O_WORKDIR
+
+# Run simulation
+echo "Job started at : "`date`
+julia --project=. --threads=1 "gs_singlerun.jl" --lambdar $lambdar --lambdaim $lambdaim --L $L
+echo "Job ended at : "`date`
---
+++ HPC stuff/CCFTlambda/separateex2sub.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+############################################################
+# Auxiliary functions                                      #
+############################################################
+
+# Function to extract values from a given line (i) in the CSV file
+extract_line() {
+    local csv_file="$1"
+    local line_number="$2"
+
+    # Read the header row to extract the variable names
+    header=$(head -n 1 "$csv_file")
+    
+    # Extract the line corresponding to the given line number (1-based index)
+    line=$(sed -n "${line_number}p" "$csv_file")
+
+    # Convert the header into an array of variables (CSV header can contain commas)
+    IFS=',' read -r -a header_array <<< "$header"
+    IFS=',' read -r -a line_array <<< "$line"
+
+    # Loop through the header and assign the corresponding value from the line
+    for x in "${!header_array[@]}"; do
+        var_name="${header_array[$x]}"
+        var_value="${line_array[$x]}"
+
+        # Remove rogue line breaks
+        cleaned_var_name=$(echo "$var_name" | tr -d '\r')
+        cleaned_var_value=$(echo "$var_value" | tr -d '\r')
+        
+        # Export the value as a variable with the header name
+        echo "$cleaned_var_name=$cleaned_var_value"
+        export "$cleaned_var_name"="$cleaned_var_value"
+    done
+}
+
+############################################################
+# Main program                                             #
+############################################################
+
+
+# Loop over all lines in the parameter csv file
+NUM_LINES=$(($(wc -l < test_values.csv) - 1))
+
+for i in $(seq 1 $NUM_LINES); do
+    echo "Submitting job for parameter values $i"
+    extract_line test_values.csv $(($i + 1))
+    qsub -N "lambda_ex2_est_sweep_$i" -V lambda_ex2_sweepscript.sh 
+done
